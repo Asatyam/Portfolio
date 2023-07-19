94afedf5765d5071e1e67952457221a28014d71a
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-07-19 23:10:41 +0530

    Change tailwind config

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -65,7 +65,7 @@ export default function Contact() {
               required
             />
             <label
-              for="email"
+              htmlFor="email"
               className="absolute text-sm dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Email
@@ -80,7 +80,7 @@ export default function Contact() {
               required
             />
             <label
-              for="message"
+              htmlFor="message"
               class="absolute text-sm dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-4 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
             >
               Message
